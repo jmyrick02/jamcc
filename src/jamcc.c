bd83fb72ee53f6e23fbf397271ff0fd34b962576
jmyrick02 <myrickj2002@gmail.com>
2023-02-02 22:51:00 -0600

    Remove unnecessary interpreter logic

--- src/jamcc.c
+++ src/jamcc.c
@@ -8,37 +8,6 @@ char* ARG_FILEPATH;
 extern FILE* GLOBAL_FILE_POINTER;
 extern FILE* LLVM_OUTPUT;
 
-int evaluate(ASTNode* root) {
-	int leftVal, rightVal;
-
-	if (root->left != NULL) {
-		leftVal = evaluate(root->left);
-	}
-	if (root->right != NULL) {
-		rightVal = evaluate(root->right);
-	}
-
-	switch (root->token.type) {
-		case PLUS:
-			return leftVal + rightVal;
-		case MINUS:
-			return leftVal - rightVal;
-		case STAR:
-			return leftVal * rightVal;
-		case SLASH:
-			return leftVal / rightVal;
-		case BITSHIFT_LEFT:
-			return leftVal << rightVal;
-		case BITSHIFT_RIGHT:
-			return leftVal >> rightVal;
-		case INTEGER_LITERAL:
-			return root->token.val;
-		default:
-			fatal(RC_ERROR, "Encountered bad operand while evaluating expression");
-			return -1;
-	}
-}
-
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
 		fatal(RC_ERROR, "You didn't pass in an input file path");
