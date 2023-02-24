450b5b88d97928c686bcbc7bc97193a9f4c231de
jmyrick02 <myrickj2002@gmail.com>
2023-02-24 17:14:44 -0600

    Change compilation target to my new OS

--- src/generate_llvm.c
+++ src/generate_llvm.c
@@ -9,9 +9,9 @@ LLVMNode* LLVM_LOADED_REGISTERS = NULL;
 extern char* ARG_FILEPATH;
 
 #define TARGET_DATALAYOUT "e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128"
-#define TARGET_TRIPLE "x86_64-redhat-linux-gnu"
+#define TARGET_TRIPLE "x86_64-pc-linux-gnu"
 #define ATTRIBUTES_0 "noinline nounwind optnone uwtable \"frame-pointer\"=\"all\" \"min-legal-vector-width\"=\"0\" \"no-trapping-math\"=\"true\" \"stack-protector-buffer-size\"=\"8\" \"target-cpu\"=\"x86-64\" \"target-features\"=\"+cx8,+fxsr,+mmx,+sse,+sse2,+x87\" \"tune-cpu\"=\"generic\""
-#define CLANG_VERSION "clang version 15.0.7 (Fedora 15.0.7-1.fc37)"
+#define CLANG_VERSION "clang version 15.0.7"
 
 void pushLoadedRegister(LLVMValue vr) {
 	LLVMNode* newNode = malloc(sizeof(LLVMNode));
