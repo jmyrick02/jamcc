f6cc434579992aa60d42d25f0d74538329c9ed61
jmyrick02 <myrickj2002@gmail.com>
2023-03-14 04:14:57 -0500

    Add testcase

---
+++ testcases/breaksnested.c
@@ -0,0 +1,16 @@
+{
+  int i;
+  int j;
+
+  for (i = 0; i < 10; i = i + 1) {
+    if (i > 5) {
+      break;
+    }
+    for (j = 0; j < 10; j = j + 1) {
+      if (j > 5) {
+        break;
+      }
+      print i + j;
+    }
+  }
+}
